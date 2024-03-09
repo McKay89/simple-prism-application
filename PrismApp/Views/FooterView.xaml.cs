b62d03b31b087dd59210dd51d5c6339cfd11787b
McKay <kg89szeged@gmail.com>
2024-03-09 13:16:24 +0100

    [ UPDATE ] SetImagePathText method implemented

--- PrismApp/Views/FooterView.xaml.cs
+++ PrismApp/Views/FooterView.xaml.cs
@@ -1,28 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace PrismApp.Views
-{
-    /// <summary>
-    /// Interaction logic for FooterView.xaml
-    /// </summary>
+{    
     public partial class FooterView : UserControl
     {
+        private static TextBlock _imagePathText = new();
+
         public FooterView()
         {
             InitializeComponent();
+            _imagePathText = Image_Path_Text;
+        }
+
+        public static void SetImagePathText(string imagePath)
+        {
+            Trace.WriteLine(imagePath);
+            _imagePathText.Text = imagePath.ToString();
         }
     }
 }
