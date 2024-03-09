97963a0e48342937b692d389c79feb9b8006250e
McKay <kg89szeged@gmail.com>
2024-03-09 13:15:48 +0100

    [ UPDATE ] SetImage method implemented

--- PrismApp/Views/BodyRightView.xaml.cs
+++ PrismApp/Views/BodyRightView.xaml.cs
@@ -1,28 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Diagnostics;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace PrismApp.Views
 {
-    /// <summary>
-    /// Interaction logic for BodyRightView.xaml
-    /// </summary>
     public partial class BodyRightView : UserControl
     {
+        private static Image _image = new();
+
         public BodyRightView()
         {
             InitializeComponent();
+            _image = Browsed_Image;
+        }
+
+        public static void SetImage(string imagePath)
+        {
+            _image.Source = new BitmapImage(new Uri(imagePath));
         }
     }
 }
